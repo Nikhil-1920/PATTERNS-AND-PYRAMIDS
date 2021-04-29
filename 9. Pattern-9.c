8ff2fc3848df934693d5d27ff6fbafb516ac5561
Nikhil Singh <66751565+Nikhil-1920@users.noreply.github.com>
2021-04-29 23:26:56 +0530

    Update 9. Pattern-9.c

--- 9. Pattern-9.c
+++ 9. Pattern-9.c
@@ -1,20 +1,19 @@
 // Jumbled equilateral triangle of numbers
-
 #include <stdio.h>
 
 int main(void) {
-    int row, k=0;
+    int row, k = 0;
     printf("Input total number of rows: ");
     scanf("%d", &row);
-    for(int i=1; i <= row; i++) {
-        for(int j=1; j <= row+i; j++) {
-            if(j >= (row+1)-i && j <= (row-1)+i && k) {
+    for(int i = 1; i <= row; i++) {
+        for(int j = 1; j <= row + i; j++) {
+            if(j >= (row + 1) - i && j <= (row - 1) + i && k) {
                 printf("%d", j);
-                k=0;
+                k = 0;
             }
             else {
                 printf(" ");
-                k=1;
+                k = 1;
             }
         }
         printf("\n");
