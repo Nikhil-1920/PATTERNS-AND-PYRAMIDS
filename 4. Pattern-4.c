4dcc894909983ca9de5b0f23fcdda57d63813d2e
Nikhil Singh <66751565+Nikhil-1920@users.noreply.github.com>
2021-04-29 23:19:08 +0530

    Update 4. Pattern-4.c

--- 4. Pattern-4.c
+++ 4. Pattern-4.c
@@ -1,11 +1,10 @@
 // Right angled consecutive numbered left triangle
-
 #include <stdio.h>
 
 int main(void) {
-    int k=0;
-    for(int i=1; i<=4; i++) {
-        for(int j=1; j<=4; j++) {
+    int k = 0;
+    for(int i = 1; i <= 4; i++) {
+        for(int j = 1; j <= 4; j++) {
             if(j <= i)
                 printf("%d", ++k);
             else 
