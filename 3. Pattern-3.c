5ecaaab1e5570a49c95e7eca32eb3d04899b2026
Nikhil Singh <66751565+Nikhil-1920@users.noreply.github.com>
2021-04-29 23:22:18 +0530

    Update 3. Pattern-3.c

--- 3. Pattern-3.c
+++ 3. Pattern-3.c
@@ -1,10 +1,9 @@
 // Right angled row numbered left triangle
-
 #include <stdio.h>
 
 int main(void) {
-    for(int i=1; i<=5; i++) {
-        for(int j=1; j<=5; j++) {
+    for(int i = 1; i <= 5; i++) {
+        for(int j = 1; j <= 5; j++) {
             if(j <= i)
                 printf("%d", i);
             else
